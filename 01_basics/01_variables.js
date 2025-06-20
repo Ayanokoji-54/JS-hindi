c8ac316df741184589c216e5ce7e9b783f310782
Ayanokoji-54 <deepakkt554499@gmail.com>
2025-06-20 08:20:26 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountaId = 144553
+let accountEmail = "aman2777827@gmail.com"
+var accountPassword = "12345" //do not use var work as universal declaration
+accountCity = "jaipur"
+let accountState;
+
+// accountid = 2 //Not allowed
+accountEmail = "agjkah@gmail.com"
+accountPassword = "63691236"
+accountCity =" bengaluru"
+
+console.log(accountaId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
