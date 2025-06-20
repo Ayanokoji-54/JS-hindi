ddae247dc4edef7fe6ef2c637199c1aeaca03135
Ayanokoji-54 <deepakkt554499@gmail.com>
2025-06-20 12:54:58 +0000

    conversion

---
+++ 01_basics/03_conversionTypes.js
@@ -0,0 +1,32 @@
+let score =undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+//true/false => 1/0
+
+let isLoggedIn = "jhgdw"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true ; 0=>false
+//" " =>false
+//"Deepak" => true
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
