93d8997e14daf95e04af9fab930e32b14a7ce570
Ayanokoji-54 <deepakkt554499@gmail.com>
2025-06-24 18:54:35 +0000

    function part 1,2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,7 +28,7 @@ function addTwoNums1(num1 , num2){
 }
 
 const result1 = addTwoNums1(6,8)
-console.log("Result : ",result1);
+// console.log("Result : ",result1);
 
 function loginUserMsg(username ){
     if(username === undefined){
@@ -39,6 +39,49 @@ function loginUserMsg(username ){
 }
 
 // console.log(loginUserMsg("Deepak"));
-console.log(loginUserMsg());
+// console.log(loginUserMsg());
 
+// case 1
+function calculateCartPrice1(num1){
+    return num1
+}
+// console.log(calculateCartPrice1(200, 300, 400));
 
+// case 2
+function calculateCartPrice2(...num2){             ///...rest or spread operator
+    return num2
+}
+// console.log(calculateCartPrice2(200 , 300, 400));
+
+// case 3
+function calculateCartPrice3(value1, value2, ...num3){
+    return num3
+}
+// console.log(calculateCartPrice3(200, 300, 400, 500));
+
+//
+const user = {
+    username: "Deepak",
+    price: 199
+}
+
+function handleObject(anyObj){
+    console.log((`User is ${anyObj.username} and price is 
+        ${anyObj.price}`));   
+}
+
+// handleObject(user)      //method 1
+handleObject({
+    username: "Sam",      //method 2
+    price: 299
+})
+
+//passing array
+ const myNewArray = [200, 300, 400, 500]
+
+ function returnValue(getArray){
+    return getArray[2]
+ }
+ console.log(returnValue(myNewArray));         //m1
+ console.log(returnValue([100,200,300,400]));  //m2
+ 
